1e48819bebf73c615001c3b11acb73f2beab3e18
Lloret <llloret@mgc.mentorg.com>
2017-03-08 16:26:20 +0000

    Check whitespace on Travis

--- utils/check_whitespace.py
+++ utils/check_whitespace.py
@@ -1,33 +1,53 @@
 '''
-Check if the files in a directory conform to whitespace rules
+Check if the files in a directory conform to whitespace rules:
+- no trailing whitespace
+- no tabs used as indentation
 '''
 import os
 from os.path import join
+import re
 
 TYPES_TO_CHECK = ['.txt', '.c', '.cpp', '.h']
 
 
-def check_line_endings(fname):
+def has_trailing_whitespace(fname):
     '''
-    true is good, false is bad
+    true if trailing whitespace is found, false otherwise
     '''
     with open(fname) as fdesc:
         for line in fdesc:
             line = line.rstrip('\n')
             if len(line) > 0:
                 if line[len(line) - 1] == ' ' or line[len(line) - 1] == '\t':
-                    return False
+                    return True
 
-    return True
+    return False
 
-bad_files = 0
-for root, dirs, files in os.walk('.'):
+def has_tabs_in_indentation(fname):
+    '''
+    true if a tab in indentation is found, false otherwise
+    '''
+    with open(fname) as fdesc:
+        for line in fdesc:
+            if re.match(r'^\s*\t', line):
+                return True
+
+    return False
+
+
+bad_files = False
+for root, dirs, files in os.walk('src'):
     for name in files:
         fullname = join(root, name)
         for end in TYPES_TO_CHECK:
             if fullname.endswith(end):
-                if not check_line_endings(fullname):
+                if has_trailing_whitespace(fullname):
                     print(fullname + " contains trailing whitespace")
-                    bad_files += 1
+                    bad_files = True
 
-exit(0 if bad_files == 0 else 1)
+                if has_tabs_in_indentation(fullname):
+                    print(fullname + " contains tabs in indentation")
+                    bad_files = True
+
+
+exit(0 if not bad_files else 1)
